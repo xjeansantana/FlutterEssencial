54221ebdeae76dd235014e52db89a9dcda80bd9d
xjeansantana <jean.santtana@gmail.com>
2020-01-02 21:29:42 -0300

    Navegando para a tela de detalhes (Dog)

--- lib/pages/hello_listview.dart
+++ lib/pages/hello_listview.dart
@@ -1,3 +1,5 @@
+import 'package:aula01/pages/dog_page.dart';
+import 'package:aula01/utils/nav.dart';
 import 'package:flutter/material.dart';
 
 class Dog {
@@ -7,12 +9,39 @@ class Dog {
   Dog(this.nome, this.foto);
 }
 
-class HelloListView extends StatelessWidget {
+class HelloListView extends StatefulWidget {
+  @override
+  _HelloListViewState createState() => _HelloListViewState();
+}
+
+class _HelloListViewState extends State<HelloListView> {
+  bool _gridView = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("ListView"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: () {
+              print("Lista");
+              setState(() {
+                _gridView = false;
+              });
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.grid_on),
+            onPressed: () {
+              print("Grid");
+              setState(() {
+                _gridView = true;
+              });
+            },
+          )
+        ],
       ),
       body: _body(),
     );
@@ -23,42 +52,58 @@ class HelloListView extends StatelessWidget {
       Dog("Jack Russel", "assets/images/dog1.png"),
       Dog("Labrador", "assets/images/dog2.png"),
       Dog("Pug", "assets/images/dog3.png"),
-      Dog("Rotweiler", "assets/images/dog4.png"),
-      Dog("Pastor", "assets/images/dog5.png"),
-      Dog("Jack Russel", "assets/images/dog1.png"),
-      Dog("Labrador", "assets/images/dog2.png"),
-      Dog("Pug", "assets/images/dog3.png"),
-      Dog("Rotweiler", "assets/images/dog4.png"),
-      Dog("Pastor", "assets/images/dog5.png"),
+      Dog("Rottweiler", "assets/images/dog4.png"),
+      Dog("Pastor", "assets/images/dog5.png")
     ];
 
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+    if (_gridView) {
+      return GridView.builder(
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemCount: dogs.length,
-        itemBuilder: (context, index) {
-          Dog dog = dogs[index];
-          return Stack(
-            fit: StackFit.expand,
-            children: <Widget>[
-              _img(dog.foto),
-              Align(
-                alignment: Alignment.topLeft,
-                child: Container(
-                  margin: EdgeInsets.all(12),
-                  padding: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Colors.black45,
-                    borderRadius: BorderRadius.circular(16)
-                  ),
-                  child: Text(
-                    dog.nome,
-                    style: TextStyle(fontSize: 26, color: Colors.white),
-                  ),
-                ),
-              )
-            ],
-          );
-        });
+        itemBuilder: (BuildContext context, int index) {
+          return _itemView(dogs, index);
+        },
+      );
+    } else {
+      return ListView.builder(
+        itemExtent: 350,
+        itemCount: dogs.length,
+        itemBuilder: (BuildContext context, int index) {
+          return _itemView(dogs, index);
+        },
+      );
+    }
+  }
+
+  _itemView(List<Dog> dogs, int index) {
+    Dog dog = dogs[index];
+
+    return GestureDetector(
+      onTap: () {
+        push(context, DogPage(dog));
+      },
+      child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          _img(dog.foto),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Container(
+              margin: EdgeInsets.all(12),
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                  color: Colors.black45,
+                  borderRadius: BorderRadius.circular(16)),
+              child: Text(
+                dog.nome,
+                style: TextStyle(fontSize: 26, color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   _img(String img) {
